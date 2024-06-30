29caa88fa05175448709556824b04fdeee54a531
Italo Seara <italo.sseara@gmail.com>
2024-06-30 19:54:11 -0300

    feat: moveRegMem

--- src/architecture/Architecture.java
+++ src/architecture/Architecture.java
@@ -395,10 +395,72 @@ public class Architecture {
    * The method reads the register id and the memory position from the memory, in positions just after the command, and
    * moves the value from the register to the memory position
    * <p>
-   * 1.
+   * 1. pc -> intbus
+   * 2. ula(1) <- intbus
+   * 3. ula.inc
+   * 4. ula(1) -> intbus
+   * 5. ula(1) -> extbus
+   * 6. pc <- intbus // pc++ (pointing to the register)
+   * 7. memory -> extbus // sets the extbus value to the id of the register
+   * 8. ula(0) <- extbus // Save the value of the extbus in the ula
+   * 9. pc -> intbus
+   * 10. ula(1) <- intbus
+   * 11. ula.inc
+   * 12. ula(1) -> intbus
+   * 13. ula(1) -> extbus
+   * 14. pc <- intbus // pc++ (pointing to the memory position)
+   * 15. memory -> extbus // sets the extbus value to the memory position
+   * 16. memory <- extbus // sends the memory position and waits for the value
+   * 17. ula(0) -> extbus // moves the value from the ula to the extbus
+   * 18. demux <- extbus // sets the demux value to the id of the register
+   * 19. registers <- extbus // this performs the internal writing of the selected register
+   * 20. memory <- extbus // sends the value to the memory and stores it in the memory position
+   * 21. pc -> intbus
+   * 22. ula(1) <- intbus
+   * 23. ula.inc
+   * 24. ula(1) -> intbus
+   * 25. pc <- intbus // pc++ (pointing to the next command) :D
    */
   public void moveRegMem() {
+    // Increment PC to point to the register
+    PC.internalRead();
+    ula.internalStore(1);
+    ula.inc();
+    ula.internalRead(1);
+    ula.read(1);
+    PC.internalStore();
 
+    // Read the register id from the memory
+    memory.read();
+    ula.store(0); // storing the register id in the ula
+
+    // Increment PC to point to the memory position
+    PC.internalRead();
+    ula.internalStore(1);
+    ula.inc();
+    ula.internalRead(1);
+    ula.read(1);
+    PC.internalStore();
+
+    memory.read(); // Get the value of the memory position
+    memory.store(); // Sends the memory position and waits for the value
+
+    // Move the value from the ula to the extbus
+    ula.read(0);
+
+    // Write the value from the ula to the selected register
+    demux.setValue(extbus.get());
+    registersRead();
+
+    // Write the value from the extbus to the memory
+    memory.store();
+
+    // Increment PC to point to the next command
+    PC.internalRead();
+    ula.internalStore(1);
+    ula.inc();
+    ula.internalRead(1);
+    PC.internalStore();
   }
 
   /**
@@ -415,7 +477,7 @@ public class Architecture {
    * 4. ula(1) -> intbus
    * 5. ula(1) -> extbus
    * 6. pc <- intbus // pc++ (pointing to the first register)
-   * 7. memory <- extbus // sets the extbus value to the id of the first register
+   * 7. memory -> extbus // sets the extbus value to the id of the first register
    * 8. ula(0) <- extbus // Save the value of the extbus in the ula (we call it GAMBIARRA)
    * 8. pc -> intbus
    * 9. ula(1) <- intbus
@@ -429,7 +491,7 @@ public class Architecture {
    * 17. pc -> intbus
    * 18. ula(1) <- intbus
    * 19. ula(1) -> extbus // MALABARISMO to move the value from the pc to the extbus
-   * 20. memory <- extbus // sets the extbus value to the id of the second register
+   * 20. memory -> extbus // sets the extbus value to the id of the second register
    * 21. demux <- extbus // sets the demux value to the id of the second register
    * 22. ula(0) -> extbus // moves the value from the ula to the extbus
    * 23. registers <- extbus // this performs the internal writing of the selected register
@@ -450,8 +512,7 @@ public class Architecture {
 
     // Read the register id from the memory
     memory.read();
-    ula.store(
-        0); // ????????????? for some reason the ula changes the value of the extbus, so we need to store it
+    ula.store(0); // storing the register id in the ula
 
     // Increment PC to point to the second register
     PC.internalRead();
@@ -507,7 +568,7 @@ public class Architecture {
    * 4. ula(1) -> intbus
    * 5. ula(1) -> extbus
    * 6. pc <- intbus // pc++ (pointing to the immediate value)
-   * 7. memory <- extbus // sets the extbus value to the immediate value
+   * 7. memory -> extbus // sets the extbus value to the immediate value
    * 8. ula(0) <- extbus // Save the value of the extbus in the ula
    * 9. pc -> intbus
    * 10. ula(1) <- intbus
@@ -515,7 +576,7 @@ public class Architecture {
    * 12. ula(1) -> intbus
    * 13. ula(1) -> extbus
    * 14. pc <- intbus // pc++ (pointing to the register)
-   * 15. memory <- extbus // sets the extbus value to the id of the register
+   * 15. memory -> extbus // sets the extbus value to the id of the register
    * 16. demux <- extbus // sets the demux value to the id of the register
    * 17. ula(0) -> extbus // moves the value from the ula to the extbus
    * 18. registers <- extbus // this performs the internal writing of the selected register
--- src/architecture/TestArchitecture.java
+++ src/architecture/TestArchitecture.java
@@ -10,6 +10,34 @@ public class TestArchitecture {
    * The following tests are for the architecture components
    */
   @Test
+  public void testMoveRegMem() {
+    Architecture arch = new Architecture();
+
+    // Making PC point to position 30
+    arch.getIntbus().put(30);
+    arch.getPC().internalStore();
+
+    // Setting the memory values
+    arch.getMemory().getDataList()[31] = 0;
+    arch.getMemory().getDataList()[32] = 100;
+
+    // Now setting the registers values
+    arch.getExtbus().put(77);
+    arch.getRegistersList().get(0).store(); //RPG0 has 77
+
+    // MOVE %rpg0 31
+    arch.moveRegMem();
+
+    // Testing if memory position 31 stores the value 77
+    assertEquals(77, arch.getMemory().getDataList()[100]);
+
+    // Testing if PC points to 3 positions after the original
+    // PC was pointing to 30; now it must be pointing to 33
+    arch.getPC().internalRead();
+    assertEquals(33, arch.getIntbus().get());
+  }
+
+  @Test
   public void testMoveRegReg() {
     Architecture arch = new Architecture();
 
@@ -23,14 +51,14 @@ public class TestArchitecture {
 
     // Now setting the registers values
     arch.getExtbus().put(77);
-    arch.getRegistersList().get(0).store(); //RPG0 has 45
+    arch.getRegistersList().get(0).store(); //RPG0 has 77
     arch.getExtbus().put(109);
-    arch.getRegistersList().get(1).store(); //RPG1 has 99
+    arch.getRegistersList().get(1).store(); //RPG1 has 109
 
     // MOVE %rpg1 %rpg0
     arch.moveRegReg();
 
-    // Testing if both REG1 and REG0 store the same value: 99
+    // Testing if both REG1 and REG0 store the same value: 109
     arch.getRegistersList().get(0).read();
     assertEquals(109, arch.getExtbus().get());
     arch.getRegistersList().get(1).read();
@@ -54,10 +82,10 @@ public class TestArchitecture {
     arch.getMemory().getDataList()[31] = 101;
     arch.getMemory().getDataList()[32] = 0;
 
-    // MOVE 77 %rpg0
+    // MOVE 101 %rpg0
     arch.moveImmReg();
 
-    // Testing if REG0 stores the value 77
+    // Testing if REG0 stores the value 101
     arch.getRegistersList().get(0).read();
     assertEquals(101, arch.getExtbus().get());
 
